39da07db8e61943d059b4c87ad783b2a971fbca4
rosario182002 <rosariodelgadomoreno17@gmail.com>
2024-10-16 16:49:34 +0200

    Add files via upload

--- JavaScript/Tema 2/Funciones/Actividad 7/js/codigo.js
+++ JavaScript/Tema 2/Funciones/Actividad 7/js/codigo.js
@@ -1,13 +1,13 @@
 let ventanita = window.open ("", "ventanita", "width=700,height=300");
-ventanita.document.write(`<p>Introduce un numero entre el 1 y 20: </p>
-                            <input type="number id="numero" min="1" max="20">
-                            <button onclick="window.opener.generarLista(document.getElementById('numero').value); window.close();">Aceptar</button>`);
-
+let num = ventanita.prompt("Dame el num del 1 al 20");
+    
 function generarLista(numero){
     let lista = "<ul>"; //creamos una lista UL
     for(let i =1; i<=numero; i++){
-        lista += "<li>" +i+ "<li>";  //creamos los elementos LI
+        lista += "<li>" +i+ "</li>";  //creamos los elementos LI
     }
-    lista += "<ul>"; //creamos una lista UL
+    lista += "</ul>"; //creamos una lista UL
     document.body.innerHTML += lista; //Insertamos la lista entera
 }
+generarLista(num);
+ventanita.window.close();
